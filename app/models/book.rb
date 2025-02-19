ec05ea2f0ac92afa0cd74511838437d0b31f6237
git_Dancunrandy <dancunrandy@gmail.com>
2025-02-19 11:46:15 +0300

    created Book Model

---
+++ app/models/book.rb
@@ -0,0 +1,11 @@
+class Book < ApplicationRecord
+  has_many :borrowings
+  has_many :users, through: :borrowings
+
+  validates :title, :author, :isbn, presence: true
+  validates :isbn, uniqueness: true
+
+  def available?
+    borrowings.where(returned_at: nil).empty?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_19_082848) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_19_083214) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "isbn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["isbn"], name: "index_books_on_isbn", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
